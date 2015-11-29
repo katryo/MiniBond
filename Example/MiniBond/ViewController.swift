104c479d0d9fba3a3283770731a4655659674bf9
Ryo Kato <katoryo55@gmail.com>
2015-11-29 22:46:28 +0900

    Add constraints

--- Example/MiniBond/ViewController.swift
+++ Example/MiniBond/ViewController.swift
@@ -2,27 +2,22 @@ import UIKit
 import MiniBond
 
 class ViewController: UIViewController {
-    var count = EventProducer<Int>(0)
+    let count = EventProducer<Int>(0)
     var countObservable: Observable<Int>?
 
     @IBOutlet weak var countLabel: UILabel!
 
     @IBAction func buttonPushed(sender: UIButton) {
-        count.value += 1
+        self.count.value += 1
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        countObservable = Observable<Int>{ [unowned self] count in
+        countObservable = Observable<Int>{ [unowned self] eventProducer in
             self.countLabel.text = String(self.count.value)
         }
         countObservable?.bindTo(count)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
 }
 
