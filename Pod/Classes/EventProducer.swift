727c24207e31ddd02a7a1e46e40bcd68f93923f0
Ryo Kato <katoryo55@gmail.com>
2016-04-24 13:06:26 +0900

    Add comment

--- Pod/Classes/EventProducer.swift
+++ Pod/Classes/EventProducer.swift
@@ -1,6 +1,14 @@
 public class EventProducer<T> {
     public var value: T {
         didSet {
+//            The filter deletes the observableboxes.
+//            
+//            When you run the app, move to the page using MiniBond, do something and then go back with the left button on the top,
+//            the view controller was created and then deleted.
+//            When the view controller was deleted, the observableBox related to the eventProducer does not be deleted. However, observableBox.observable becomes nil. This is because ObservableBox.observable is weak reference.
+//            
+//            When you repeat the action that go to the page and go back, there will be a lot of empty ObservableBoxes. They do not harm except for a little extra memory usage. I did not want to allow extra usage of memory, so I added the filter.
+
             observableBoxes = observableBoxes.filter(
                 {
                     (observableBox: ObservableBox<T>) -> Bool in
